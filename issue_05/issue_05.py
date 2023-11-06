662912f3ac03fe8a4d8224b77d568473aee01360
IvanVas <vanya71161@gmail.vom>
2023-11-06 17:06:30 +0300

    issues_01-05

---
+++ issue_05/issue_05.py
@@ -0,0 +1,66 @@
+"""
+Get current data test
+"""
+import urllib.request
+import io
+import pytest
+import json
+from unittest.mock import patch
+
+
+API_URL = 'http://worldclockapi.com/api/json/utc/now'
+
+YMD_SEP = '-'
+YMD_SEP_INDEX = 4
+YMD_YEAR_SLICE = slice(None, YMD_SEP_INDEX)
+
+DMY_SEP = '.'
+DMY_SEP_INDEX = 5
+DMY_YEAR_SLICE = slice(DMY_SEP_INDEX + 1, DMY_SEP_INDEX + 5)
+
+
+def what_is_year_now() -> int:
+    """
+    Получает текущее время из API-worldclock и извлекает из поля 'currentDateTime' год
+    Предположим, что currentDateTime может быть в двух форматах:
+      * YYYY-MM-DD - 2019-03-01
+      * DD.MM.YYYY - 01.03.2019
+    """
+    with urllib.request.urlopen(API_URL) as resp:
+        resp_json = json.load(resp)
+
+    datetime_str = resp_json['currentDateTime']
+    if datetime_str[YMD_SEP_INDEX] == YMD_SEP:
+        year_str = datetime_str[YMD_YEAR_SLICE]
+    elif datetime_str[DMY_SEP_INDEX] == DMY_SEP:
+        year_str = datetime_str[DMY_YEAR_SLICE]
+    else:
+        raise ValueError('Invalid format')
+
+    return int(year_str)
+
+
+def test_year_ymd():
+    data = io.StringIO('{"currentDateTime": "2023-11-06"}')
+
+    with patch.object(urllib.request, 'urlopen', return_value=data):
+        assert what_is_year_now() == 2023
+
+
+def test_year_dmy():
+    data = io.StringIO('{"currentDateTime": "06.11.2023"}')
+
+    with patch.object(urllib.request, 'urlopen', return_value=data):
+        assert what_is_year_now() == 2023
+
+
+def test_year_exception():
+    data = io.StringIO('{"currentDateTime": "066.111.2023"}')
+
+    with patch.object(urllib.request, 'urlopen', return_value=data):
+        with pytest.raises(ValueError):
+            assert what_is_year_now() == 2023
+
+
+if __name__ == '__main__':
+    pass
